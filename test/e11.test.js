3834fa0d09dff18b42a1bac2e1b8f9da446a65ad
Jonathan Ramirez <77942993+jlramirez1234@users.noreply.github.com>
2023-01-12 21:51:34 -0600

    initial

---
+++ test/e11.test.js
@@ -0,0 +1,20 @@
+import { describe, expect, it } from "vitest";
+import { lowMoonsPlanets } from "../exercises/e11";
+import { data } from "../data/data";
+
+describe("lowMoonsPlanets", () => {
+  it("Should exist", () => {
+    expect(lowMoonsPlanets).toBeInstanceOf(Function);
+  });
+  it("Should return an array of 2 items", () => {
+    expect(lowMoonsPlanets(data).length).toEqual(4);
+  });
+  it("The items names should be 'Mars' and 'Earth'", () => {
+    expect(lowMoonsPlanets(data)).toEqual([
+      "Mars",
+      "Mercure",
+      "Earth",
+      "Vénus",
+    ]);
+  });
+});
